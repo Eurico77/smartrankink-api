2a1b82457bb11557cb0fddf9ba85a5de1409367c
eurico77 <euricomagalhaes61@gmail.com>
2023-08-09 21:37:59 -0300

    feature: create category modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from './categories/categories.module';
 const DB_CONNECTION =
   'mongodb://root:root@localhost:27017/smartranking?authSource=admin';
 
 @Module({
-  imports: [MongooseModule.forRoot(DB_CONNECTION), PlayersModule],
+  imports: [MongooseModule.forRoot(DB_CONNECTION), PlayersModule, CategoriesModule],
   controllers: [],
   providers: [],
 })
